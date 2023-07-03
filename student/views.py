773ff3dd8ec8299337095edaafdae18d2120f82a
Arpan <me.arpan122@gmail.com>
2023-07-03 23:47:32 +0530

    add logout features, improve UI, add javascript, fix broken urls

--- student/views.py
+++ student/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect, reverse
 from . import forms, models
+
+
 from django.db.models import Sum
 from django.contrib.auth.models import Group
+
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.conf import settings
+
 from datetime import date, timedelta
+
 from exam import models as QMODEL
 from teacher import models as TMODEL
 
